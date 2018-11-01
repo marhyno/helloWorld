4fa00028e2ee42992ccef67027aa5ecbba9129cf
marhyno <stefan.marcin74@gmail.com>
2018-11-01 23:10:33 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,81 @@
+﻿<!DOCTYPE html>
+<html class="html" lang="sk-SK">
+ <head>
+  
+ <link href="css/new_style.css" rel="stylesheet" type="text/css">
+<div id="fb-root"></div>
+<script>
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/sk_SK/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+</head>
+<body>
+    <div class="logo">
+        <img src="images/logonew2.png" style="z-index:5;" width="170px" height="170px" alt="logo_nove">
+</div>
+    <a href="https://www.facebook.com/pages/Naša-Kaša-Vaše-zdravie/566826110122806?fref=ts" target="_blank">
+    	<div class="facebook">
+        </div>
+    </a>
+    <a href="mailto:nasakasa@nasakasa.sk">
+    <div id="email"><img src="images/envelope.png">
+    <span>nasakasa@nasakasa.sk</span>
+    </div>
+    </a>
+ 
+    <div class="menu">
+    <a href="co-obsahuje.html">
+   	  <div class="item1">
+      		<div class="item_name">Čo obsahuje</div>
+      </div>
+     </a>
+     <a href="kde-kupit.html">
+		<div class="item2">
+    		<div class="item_name">Kde kúpiť</div>
+    	</div>
+     </a>
+     <a href="kontakt.html">
+        <div class="item3">
+          	<div class="item_name">Kontakt</div>
+         </div>
+     </a>
+    </div>
+ 
+<div class="main">
+        <div class="main_picture">
+        <img src="images/IMG_6632_edited.png" width="27%" height="28%" alt="Nase Kase">
+	    <img src="images/IMG_6627_edited.png" width="29%" height="29%" alt="Nase Kase"> 
+        </div>
+</div>
+<div class="nadpisy">
+	<div class="nadpis1">
+    Raňajkova cereálna bezlepková kaša
+    </div>
+    <div class="nadpis2">
+    Večerná proteínovo-cereálna bezlepková kaša
+    </div>
+</div>
+<div class="popisy">
+        <div class="popisy_vlavo">
+        <ul class="vlavo" dir="rtl" type="disc">
+        <li>Najlepší štart dňa</li>
+        <li>Energia bez záťaže</li>
+        <li>Nutrične bohatá</li>
+        </ul>
+        </div>
+        <div class="popisy_vpravo">
+        <ul class="vpravo">
+        <li>Pre kvalitnú regeneráciu</li>
+        <li>Upokojenie žalúdka </li>
+		<li>Pokojný spánok</li>
+        </ul>
+        </div>
+		</div>
+</body>
+
+</html>
---
+++ visit_counter.php
@@ -0,0 +1,21 @@
+﻿<link href="css/index.css" rel="stylesheet" type="text/css" />
+<?php
+
+     $hostname = "nasakasa.sk";
+     $dbuser = "nasakasa_sk"; 
+     $dbpassword = "Kzza73YRtw6CTCAC"; 
+     $dbname = "nasakasa_sk";
+
+	$db_link=mysql_connect($hostname, $dbuser, $dbpassword) or die("Nepodarilo sa napojiť na server!");
+
+   mysql_select_db($dbname) or die("Nepodarilo sa napojiť k SQL databáze");
+ 
+ mysql_query("UPDATE visitors_count SET views=views+1 WHERE page='index'");
+ $result = mysql_query("SELECT count FROM visitors_count WHERE page='index'");
+ while($row = mysql_fetch_row($result))
+ 
+ {
+	 echo "<div style='text-align:right'; 'vertical-align: text-bottom;'> Počet kliknutí:" . $row['0']."</div>";
+ }
+   
+?>
